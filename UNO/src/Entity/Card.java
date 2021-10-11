d234576eed4a19e2144a2a3f3c89a4a7b2064427
Yuezhexuan Zhu <zhuyuezx@gmail.com>
2021-10-11 16:22:29 -0400

    zhuyuezx's first commit, with basics of player constructed and skeleton part of playerUseCase created. Also modified other two entities.

--- UNO/src/Entity/Card.java
+++ UNO/src/Entity/Card.java
@@ -1,3 +1,5 @@
+package Entity;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -14,13 +16,19 @@ public class Card {
      * giving the card the given color, number, function
      *
      * @param color   The card's color <red, blue, yellow, green, black>
-     * @param number   The card's number <0~9, null>
+     * @param number   The card's number <0~9, -1>
      * @param function    The card's function <switch, reverse, skip, plustwo, plusfour, null>
      */
     public Card(String color, int number, String function){
         this.color = color;
         this.number = number;
         this.function = function;
+    }
+
+    public Card(){
+        this.color = "black";
+        this.number = -1;
+        this.function = null;
     }
 
     /**
@@ -31,15 +39,22 @@ public class Card {
     }
 
     public Object getNumber(){
-        if (this.number == 'null'){
+        if (this.number == -1){
             return null;
         }
         return this.number;
     }
 
-    public Object getFunction(){
-        if (this.function == 'null'){
+    public Object getFunction() {
+        if (this.function == null) {
             return null;
+        }
         return this.function;
     }
+
+    // test code
+//    public static void main(String[] args){
+//        Card c1 = new Card("red", 2, null);
+//        System.out.println(c1.getColor());
+//    }
 }
