14e4c3d556ee05f6f0fd1370ff9f8163c8e8df8c
ElizaZoldyck <eliza.gao@mail.utoronto.ca>
2021-10-13 14:02:03 -0400

    Drafted Card class, will modify later

--- UNO/src/Card.java
+++ UNO/src/Card.java
@@ -5,8 +5,8 @@ import java.util.Map;
 
 public class Card {
     private String color;
-    private int number;
-    private String function;
+    private Object number;
+    private Object function;
 
     /**
      * Construct the individual card with info implemented in UNO card game.
@@ -17,7 +17,7 @@ public class Card {
      * @param number   The card's number <0~9, null>
      * @param function    The card's function <switch, reverse, skip, plustwo, plusfour, null>
      */
-    public Card(String color, int number, String function){
+    public Card(String color, Object number, Object function){
         this.color = color;
         this.number = number;
         this.function = function;
@@ -31,15 +31,16 @@ public class Card {
     }
 
     public Object getNumber(){
-        if (this.number == 'null'){
+        if (this.number == null){
             return null;
         }
         return this.number;
     }
 
     public Object getFunction(){
-        if (this.function == 'null'){
+        if (this.function == null) {
             return null;
+        }
         return this.function;
     }
 }
