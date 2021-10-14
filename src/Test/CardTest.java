b6f0908f3fd39e5cbf1b5a49a03e340374676279
Yuezhexuan Zhu <zhuyuezx@gmail.com>
2021-10-13 23:47:26 -0400

    zhuyuezx's first commit, with basics of player constructed and skeleton part of playerUseCase created. Also modified other two entities.

--- src/Test/CardTest.java
+++ src/Test/CardTest.java
@@ -9,12 +9,6 @@ public class CardTest {
 //    Card c1;
 //    Card c2;
 
-    @Before
-    public void setUp() throws Exception {
-        Card c1 = new Card("red", 7, null);
-        Card c2 = new Card("black", -1, "+4");
-    }
-
     @Test
     public void TestGetColor() {
         assertEquals("red", c1.getColor());
