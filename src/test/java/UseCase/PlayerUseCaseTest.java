1d7139e344f28a7914146a020f8e793e779df3e5
Yuezhexuan Zhu <zhuyuezx@gmail.com>
2021-10-15 00:41:52 -0400

    implemented PlayerUseCaseTest.java with some basic tests

--- src/test/java/UseCase/PlayerUseCaseTest.java
+++ src/test/java/UseCase/PlayerUseCaseTest.java
@@ -1,4 +1,50 @@
 package UseCase;
 
+import Entity.Card;
+import Entity.Player;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PlayerUseCaseTest {
+
+    @Test
+    public void testgetPlayers(){
+        PlayerUseCase pManager = new PlayerUseCase(4);
+        Player[] p = pManager.getPlayers();
+        assertEquals(4, p.length);
+    }
+
+    @Test
+    public void testCreatePlayer(){
+        PlayerUseCase pManager = new PlayerUseCase(4);
+        for (int i = 0; i < 4; i++){
+            pManager.createPlayer(Integer.toString(i), i);
+        }
+        assertEquals("3", pManager.getPlayers()[3].getId());
+    }
+
+    @Test
+    public void testPlayerDrawCard(){
+        PlayerUseCase pManager = new PlayerUseCase(4);
+        for (int i = 0; i < 4; i++){
+            pManager.createPlayer(Integer.toString(i), i);
+        }
+        pManager.playerDrawCard(0, new Card("red", "red6"));
+        assertEquals(pManager.getPlayers()[0].getHandCard().get(0).getId(), "red6");
+    }
+
+    @Test
+    public void testPlayerPlayCard(){
+        PlayerUseCase pManager = new PlayerUseCase(4);
+        for (int i = 0; i < 4; i++){
+            pManager.createPlayer(Integer.toString(i), i);
+        }
+        Card toPlayed = new Card("red", "red6");
+        pManager.playerDrawCard(0, toPlayed);
+        Card played = pManager.playerPlayCard(0, toPlayed);
+        assertEquals(played, toPlayed);
+    }
 }
