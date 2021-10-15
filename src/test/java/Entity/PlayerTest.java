feb25302f5ea02cc451ec51b8a14ae96b661c20b
wisechua <wise.chua@mail.utoronto.ca>
2021-10-14 22:59:34 -0400

    Basic Unittest for Player Class

---
+++ src/test/java/Entity/PlayerTest.java
@@ -0,0 +1,58 @@
+
+package Entity;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PlayerTest {
+
+    @Test
+    public void TestdrawCard() {
+        Player p1 = new Player("player1", 1);
+        Card c1 = new NumberCard("yellow", 6, "1");
+        p1.drawCard(c1);
+        assertTrue(p1.getHandCard().contains(c1));
+    }
+
+    @Test
+    public void TestplayCard() {
+        Player p1 = new Player("player1", 1);
+        Card c1 = new NumberCard("yellow", 6, "1");
+        p1.drawCard(c1);
+        assertEquals(c1, p1.playCard(c1));
+        assertEquals(new Card().getColor(), p1.playCard(c1).getColor());
+        // Need to mute return message
+    }
+
+    @Test
+    public void TestgetCardNum() {
+        Player p1 = new Player("player1", 1);
+        Card c1 = new NumberCard("yellow", 6, "1");
+        assertEquals(0, p1.getCardNum());
+        p1.drawCard(c1);
+        assertEquals(1, p1.getCardNum());
+    }
+
+    @Test
+    public void TestgetHandCard() {
+        Player p1 = new Player("player1", 1);
+        assertEquals(0, p1.getHandCard().size());
+        assertTrue(p1.getHandCard().isEmpty());
+    }
+
+    @Test
+    public void TestgetID() {
+        Player p1 = new Player("player1", 1);
+        assertEquals("player1", p1.getId());
+    }
+
+    @Test
+    public void TestgetPosition() {
+        Player p1 = new Player("player1", 1);
+        assertEquals(1, p1.getPosition());
+    }
+
+}
