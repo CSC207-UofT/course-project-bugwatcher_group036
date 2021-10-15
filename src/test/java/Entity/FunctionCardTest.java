374f4043dfb4aad3aafbab747ceab5d966f4df46
wisechua <wise.chua@mail.utoronto.ca>
2021-10-14 23:26:31 -0400

    Basic Unittest for FunctionCard Class

--- src/test/java/Entity/FunctionCardTest.java
+++ src/test/java/Entity/FunctionCardTest.java
@@ -1,4 +1,17 @@
 package Entity;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class FunctionCardTest {
+
+    @Test
+    public void TestgetFunction() {
+        Player p1 = new Player("player1", 1);
+        FunctionCard c1 = new FunctionCard("yellow", "id", "switch");
+        assertEquals("switch", c1.getFunction());
+    }
 }
