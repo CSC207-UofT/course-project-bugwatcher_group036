848699249d81205b824ee2226b9f73f7c539b54c
Jialun Chen <779801187@qq.com>
2021-11-10 00:56:36 -0500

    login

--- src/main/java/Entity/NumberCard.java
+++ src/main/java/Entity/NumberCard.java
@@ -6,20 +6,28 @@ public class NumberCard extends Card {
 
     /**
      *
-     * @param color
+     * @param color The color of the card <red, yellow, green, blue>
      * @param number   The card's number <0~9, -1>
-     * @param id
+     * @param id    The if of the card.
      */
     public NumberCard(String color, int number, String id) {
         super(color, id);
         this.number = number;
     }
 
+    /**
+     * The default value for number card.
+     */
     public NumberCard() {
         super();
         this.number = -1;
     }
 
+    /**
+     * Get the number of this card.
+     *
+     * @return An int represent the number of the card.
+     */
     public int getNumber(){
         return number;
     }
--- src/main/java/LogIn/UserData.java
+++ src/main/java/LogIn/UserData.java
@@ -3,12 +3,23 @@ package LogIn;
 import java.util.ArrayList;
 
 public class UserData implements UserOperation{
-    static ArrayList<User> users = new ArrayList<User>();
+    static ArrayList<User> users = new ArrayList<>();
 
+    /**
+     * Add an username to the system.
+     *
+     * @param user The User who want to play this game.
+     */
     public void register(User user){
         users.add(user);
     }
 
+    /**
+     * return true is both username and password are right.
+     *
+     * @param user The User tries to log in.
+     * @return If the username correspond to password or not.
+     */
     public boolean logIn(User user){
         boolean flag = false;
         for (User user0 : users){
