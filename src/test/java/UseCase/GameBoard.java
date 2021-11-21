f44c8ce7921305ffd9922ea3adc32d8a5b0b9baa
ElizaZoldyck <eliza.gao@mail.utoronto.ca>
2021-11-20 21:08:56 -0500

    temp save

---
+++ src/test/java/UseCase/GameBoard.java
@@ -0,0 +1,11 @@
+package UseCase;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameBoard {
+}
