b6d5bd7b9702b2d8ebde7915cb6f83137b9b2041
Jialun Chen <779801187@qq.com>
2021-11-22 19:07:45 -0500

    test for eachround

--- src/test/java/Controller/EachRoundTest.java
+++ src/test/java/Controller/EachRoundTest.java
@@ -1,5 +1,40 @@
 package Controller;
 
+import Entity.Deck;
+import Entity.HandCard;
+import Entity.Status;
+import UseCase.CardChecker;
+import UseCase.Dealer;
+import UseCase.GameBoard;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class EachRoundTest {
+    Deck deck = new Deck();
+    GameBoard gameBoard = new GameBoard(4);
+    Dealer dealer = new Dealer(deck);
+    CardChecker cardChecker;
+    ITerminal iTerminal;
+
+
+    @Test
+    public void testInitialization(){
+        EachRound eachRound = new EachRound(gameBoard, dealer, cardChecker);
+        assertEquals(eachRound.getCardChecker(), cardChecker);
+
+    }
+
+    @Test
+    public void testGetGameBoard() {
+    }
+
+    @Test
+    public void testGetTerminal() {
+    }
+
+    @Test
+    public void testGetCardChecker() {
+    }
 }
