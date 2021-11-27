0026553e8591819fa4d84db88681f2a06d6256ac
Jialun Chen <779801187@qq.com>
2021-11-26 20:02:16 -0500

    testForGameStatus

--- src/test/java/UseCase/GameCardHoldersTest.java
+++ src/test/java/UseCase/GameCardHoldersTest.java
@@ -1,4 +1,11 @@
 package UseCase;
 
+import Entity.Deck;
+import Entity.CardHolder;
+import Entity.Status;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class GameCardHoldersTest {
 }
