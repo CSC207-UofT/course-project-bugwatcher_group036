21287c1b7d64d361c47c4521f140b7db55252bbe
wisechua <wise.chua@mail.utoronto.ca>
2021-12-05 17:25:01 -0500

    Eachround docstring

--- src/main/java/UseCase/EachRound.java
+++ src/main/java/UseCase/EachRound.java
@@ -30,6 +30,7 @@ public class EachRound {
 
     /**
      * Getter method for gameBoard
+     * @return the GameBoard for the uno game
      */
     public GameBoard getGameBoard() {
         return gameBoard;
@@ -37,6 +38,7 @@ public class EachRound {
 
     /**
      * Getter method for iPresenter
+     * @return the iPresenter for the uno game
      */
     public IPresenter getTerminal() {
         return iPresenter;
@@ -44,12 +46,13 @@ public class EachRound {
 
     /**
      * Getter method for gameRequest
+     * @return the GameRequest for the uno game
      */
     public GameRequest getGameRequest() {return gameRequest;}
 
     /**
      * Start the round for current player. If last card is plus, then let the player draw cards or play plus card.
-     * return the cards that current player can play
+     * @return the cards that current player can play
      */
     public CardHolder beginStage() {
         int currentPlayerIndex = gameBoard.getGameStatus().getCurrentPlayerIndex();
@@ -72,6 +75,10 @@ public class EachRound {
         }
     }
 
+    /**
+     * The playstage when a player played a card that is playable.
+     * To check whether the player have any card or not.
+     */
     public void playStageGUI(CardHolder playableCards, int currentPlayerIndex,
                              String cardToPlay, UserStatistics stats) {
         if (gameBoard.getGameCardHolders().isEmpty(playableCards)) {
@@ -94,6 +101,12 @@ public class EachRound {
         }
     }
 
+    /**
+     * The playstage for the computer.
+     * @param playableCards The cards that the computer player can play
+     * @param currentPlayerIndex the index of the current player.
+     * @return the card that the computer played
+     */
     public String playStageGUIPVE(CardHolder playableCards, int currentPlayerIndex) {
         String cardToPlay = null;
         if (gameBoard.getGameCardHolders().isEmpty(playableCards)) {
@@ -110,6 +123,11 @@ public class EachRound {
         return cardToPlay;
     }
 
+    /**
+     * The endstage when a player play a card. To set all the game and status to the correct type.
+     * @param toPlay The card that played by last player.
+     * @param stats The informaiton statistics of the player
+     */
     public void endStageGUI(String toPlay, UserStatistics stats) {
 
         gameBoard.getGameStatus().setSkip(false);
@@ -124,6 +142,11 @@ public class EachRound {
         gameBoard.getGameStatus().setCurrentPlayerIndex(gameBoard.getGameStatus().moveToNextPlayer());
     }
 
+    /**
+     * The endstage when a computer play a card. To set all the game and status to the correct type.
+     * @param toPlay The card that played by Computer.
+     * @param currentPlayerIndex The computer player index
+     */
     public void endStageGUIPVE(String toPlay, int currentPlayerIndex) {
         if (currentPlayerIndex == 0) {
             endStageGUI(toPlay, new UserStatistics("noSave"));
@@ -144,36 +167,31 @@ public class EachRound {
         }
     }
 
+    /**
+     * For player to play a card when the card is playable or draw a card.
+     * @param currentPlayerIndex the index of the current player
+     * @param cardToPlayID the card that the player played or draw a card.
+     * @return the card either the player draw or the card that player played.
+     */
     public String letPlayerPlayCardGUI(int currentPlayerIndex, String cardToPlayID) {
-        String cardToPlay = null;
-
-        // rightCard indicates whether the play type a right card to play.
-        boolean rightCard;
-
-        // The number of times that the player type a wrong card.
-        int wrongTimes = 0;
-
-        // Let the player type the card to play. If type a wrong card, type again,
-        // with maximum 3 times.
-        do {
-            if (cardToPlayID.equals("draw")) {
-                gameBoard.getGameCardHolders().addCard(
-                        gameBoard.drawCardWithNotification(false, false), currentPlayerIndex);
-                cardToPlay = "white -1";
-                return cardToPlay;
+        String cardToPlay;
+        if (cardToPlayID.equals("draw")) {
+            gameBoard.getGameCardHolders().addCard(
+                    gameBoard.drawCardWithNotification(false, false), currentPlayerIndex);
+            cardToPlay = "white -1";
+            return cardToPlay;}
+        else {
+            cardToPlay = cardToPlayID;
             }
-            if (!gameBoard.getGameCardHolders().playCard(cardToPlayID, currentPlayerIndex)) {
-                wrongTimes++;
-                rightCard = false;
-            } else {
-                rightCard = true;
-                cardToPlay = cardToPlayID;
-            }
-        } while (!rightCard && wrongTimes < 3);
-        //exit when the player types the right class or wrong time exceed 3
         return cardToPlay;
     }
 
+    /**
+     * For computer to play a card when there is playable card
+     * @param playableCards the cards that the computer can play
+     * @param currentPlayerIndex the index of the current player
+     * @return the card that the computer play
+     */
     public String letPlayerPlayCardForComputer(CardHolder playableCards, int currentPlayerIndex) {
         String cardToPlay;
 
@@ -184,7 +202,8 @@ public class EachRound {
     }
 
     /**
-     * @return get the current index of the player
+     * Getter method for the current player index.
+     * @return the current index of the player
      */
     public int getCurrentPlayerIndex() {
         return getGameBoard().getCurrentPlayerIndex();
